c3aaa45ad25b8333236bcd1f49edb771b51ade3a
Somansh-Bodapati <somansh.bodapati@gmail.com>
2024-11-09 22:42:44 -0600

    Created different header components and linked them together to display in sequence

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from './header/header.component';
+import {TopHeaderComponent} from './top-header/top-header.component';
+import {TopMenuComponent} from './header/top-menu/top-menu.component';
 
+// @ts-ignore
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   imports: [
-    HeaderComponent
+    TopHeaderComponent,
+    HeaderComponent,
+    TopMenuComponent
   ],
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angular-ekart';
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,15 @@
 import {Component} from '@angular/core';
+import {TopHeaderComponent} from "../top-header/top-header.component";
+import {TopMenuComponent} from "./top-menu/top-menu.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.component.html',
+  imports: [
+      TopMenuComponent,
+    TopHeaderComponent
+  ],
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
