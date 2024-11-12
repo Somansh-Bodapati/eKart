37664c56dd4458dc9d6c469c30ee385a42da15f7
Somansh-Bodapati <somansh.bodapati@gmail.com>
2024-11-12 13:33:52 -0600

    Created product list and its entries

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import {SearchComponent} from './search/search.component';
 import {NgForOf} from '@angular/common';
+import {ProductListComponent} from './product-list/product-list.component';
 
 
 @Component({
   selector: 'container',
   standalone: true,
-  imports: [SearchComponent, NgForOf],
+  imports: [SearchComponent, NgForOf, ProductListComponent],
   templateUrl: './container.component.html',
   styleUrl: './container.component.css'
 })
