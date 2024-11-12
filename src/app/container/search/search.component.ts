f5acc84ee5b01a80e8b0f66c1cfa0adade1be271
Somansh-Bodapati <somansh.bodapati@gmail.com>
2024-11-12 15:02:37 -0600

    ngIf first example

--- src/app/container/search/search.component.ts
+++ src/app/container/search/search.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'product-search',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    NgIf
   ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
-  searchText: string= 'Womens watch';
+  searchText: string= ' ';
 
   updateSearchText(event: any){
     this.searchText = event.target.value;
