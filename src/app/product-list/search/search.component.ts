9c6ab62f17819d6b399966f54c6195300ea6ac6b
Somansh-Bodapati <somansh.bodapati@gmail.com>
2024-11-11 20:54:08 -0600

    Two way binding using NgModel directive

--- src/app/product-list/search/search.component.ts
+++ src/app/product-list/search/search.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'product-search',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
